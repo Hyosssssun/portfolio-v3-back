16353d8c933428aca5b080f48668e4b0c43fc1d5
Hyosssssun <hyosssssun@gmail.com>
2024-01-13 23:52:46 +0000

    create models

---
+++ back/models/products.js
@@ -0,0 +1,22 @@
+/* GET ALL PRODUCTS */
+async function getAllProducts() {}
+
+/* GET A PRODUCT BY ID */
+async function getProductById(id) {}
+
+/* ADD A PRODUCT */
+async function addProduct() {}
+
+/* UPDATE A PRODUCT */
+async function updateProductById(id) {}
+
+/* DELETE A PRODUCT */
+async function deleteProductById(id) {}
+
+export {
+  getAllProducts,
+  getProductById,
+  addProduct,
+  updateProductById,
+  deleteProductById,
+};
