dc61a39f1b1f2c6f2532e68fb386c880eda31ff0
Hyosssssun <hyosssssun@gmail.com>
2024-01-13 23:49:57 +0000

    express setup

---
+++ back/app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+
+import __dirname from './dirname.js';
+import productsRouter from './routes/products.js';
+
+const app = express();
+const port = 8080;
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/products', productsRouter);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'ok' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.use(function (req, res, next) {
+  res
+    .status(404)
+    .json({ message: "We couldn't find what you were looking for 😞" });
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.message, err.stack);
+  res.status(500).json(err);
+});
+
+export default app;
---
+++ back/config.js
@@ -0,0 +1,7 @@
+const config = {
+  uri: process.env.URI,
+  username: process.env.DATABASE_USER_USERNAME,
+  password: process.env.DATABASE_USER_PASSWORD,
+};
+
+export default config;
