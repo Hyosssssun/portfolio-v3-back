6624bab12f2deb1b59ab85521e6bfab24528dcff
Hyosssssun <hyosssssun@gmail.com>
2024-01-14 23:48:06 +0000

    get rid or models and handle in router

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,41 +1,94 @@
 import express from 'express';
-import database from '../db/connection.js';
+import database, { closeConnection } from '../db/connection.js';
+import { ObjectId } from 'mongodb';
 
 const router = express.Router();
+const productsCollection = database.collection('products');
 
 /* GET ALL PRODUCTS */
-router.get('/', async (req, res) => {
-  const allProduct = await database
-    .collection('products')
-    .findOne({ stock_number: '12345' });
+router.get('/', async (_, res) => {
+  try {
+    const products = await productsCollection.find({}).toArray();
+    console.log('-----in new get', products);
 
-  // Print the document returned by findOne()
-  console.log('all product', allProduct);
-  return allProduct;
+    res.send(products).status(200);
+  } catch (err) {
+    console.error(`Something is wrong here`, err.message);
+  } finally {
+    await closeConnection();
+  }
 });
 
 /* GET A PRODUCT */
-router.get('/:id', (req, res) => {});
+router.get('/:id', async (req, res) => {
+  console.log(req.params);
+  const query = { _id: new ObjectId(req.params.id) };
+
+  try {
+    const product = await productsCollection.findOne(query);
+    console.log('-----in new getById', product);
+
+    if (!product) {
+      res.send('Not found').status(404);
+    } else {
+      res.send(product).status(200);
+    }
+  } catch (err) {
+    console.error(`Something is wrong here`, err.message);
+  } finally {
+    await closeConnection();
+  }
+});
 
 /* ADD A PRODUCT */
 router.post('/', async (req, res) => {
-  // Create a document to insert
-  const doc = {
-    stock_number: '12345',
-    name: 'Pro Batteries',
-    Description: 'Batteries',
-    Price: '£1.99',
-  };
-  // Insert the defined document into the "haiku" collection
-  const result = await database.collection('mongodb_fullstack').insertOne(doc);
-  // Print the ID of the inserted document
-  console.log(`A document was inserted: ${result.insertedId}`);
+  let newDocument = req.body;
+
+  try {
+    let product = await productsCollection.insertOne(newDocument);
+    console.log('-----in new post', product);
+
+    res.send(product).status(204);
+  } catch (err) {
+    console.error(`Something is wrong here`, err.message);
+  } finally {
+    await closeConnection();
+  }
 });
 
 /* UPDATE A PRODUCT */
-router.put('/:id', (req, res) => {});
+router.put('/:id', async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const updates = {
+    $push: req.body,
+  };
+
+  try {
+    let product = await productsCollection.updateOne(query, updates);
+    console.log('-----in new update', product);
+
+    res.send(product).status(200);
+  } catch (err) {
+    console.error(`Something is wrong here`, err.message);
+  } finally {
+    await closeConnection();
+  }
+});
 
 /* DELETE A PRODUCT */
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+
+  try {
+    let product = await productsCollection.deleteOne(query);
+    console.log('-----in new delete', product);
+
+    res.send(product).status(200);
+  } catch (err) {
+    console.error(`Something is wrong here`, err.message);
+  } finally {
+    await closeConnection();
+  }
+});
 
 export default router;
