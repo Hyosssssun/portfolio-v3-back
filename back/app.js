d5810b8169635ae898cf6dd540719a8b3608740f
Hyosssssun <hyosssssun@gmail.com>
2024-02-25 23:05:53 +0000

    export server

--- back/app.js
+++ back/app.js
@@ -34,7 +34,7 @@ app.use(function (err, req, res, next) {
   res.status(500).json(err);
 });
 
-app.listen(port, () => {
+export const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
