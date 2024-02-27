8514ed4d9cb2953f483be7195981cb21f3575d86
Hyosssssun <hyosssssun@gmail.com>
2024-02-27 22:45:40 +0000

    update test

--- back/routes/products.test.js
+++ back/routes/products.test.js
@@ -1,124 +1,129 @@
 import request from 'supertest';
 import app, { server } from '../app.js';
-import { database, connectDB, disconnectDB } from '../db/connection.js';
+import { database, disconnectDB } from '../db/connection.js';
 
 describe('products', () => {
   let productsCollection;
-  beforeAll(async () => {
-    connectDB();
-    // productsCollection = database.collection('products');
-    const initialData = [
-      {
-        _id: 'test-id-1',
-        stock_number: '1',
-        name: 'test-name-1',
-        Description: 'test-description-1',
-        Price: 'test-price-1',
-      },
-      {
-        _id: 'test-id-2',
-        stock_number: '2',
-        name: 'test-name-2',
-        Description: 'test-description-2',
-        Price: 'test-price-2',
-      },
-    ];
-    // await productsCollection.insertMany(initialData);
+
+  const initialData = [
+    {
+      _id: 'test_id_1',
+      stock_number: '1',
+      name: 'test_name_1',
+      Description: 'test_description_1',
+      Price: 'test_price_1',
+    },
+    {
+      _id: 'test_id_2',
+      stock_number: '2',
+      name: 'test_name_2',
+      Description: 'test_description_2',
+      Price: 'test_price_2',
+    },
+  ];
+
+  beforeAll(() => {
+    productsCollection = database.collection('products');
+  });
+
+  beforeEach(async () => {
+    await productsCollection.deleteMany({});
+    await productsCollection.insertMany(initialData);
   });
 
   afterAll(async () => {
-    disconnectDB();
+    await disconnectDB();
     server.close();
   });
 
-  it.only('GET /products', async () => {
+  it('GET /products', async () => {
     // fetch get all products
-    const total = await request(app).get('/products').expect(200);
-    // console.log(await productsCollection.find({}).toArray());
-    console.log('*** total', total);
+    const allProducts = await request(app).get('/products');
 
-    // describe('GET /api/products/:id', () => {
-    //   it('should return a product', async () => {
-    //     const res = await request(app).get(
-    //       '/api/products/6331abc9e9ececcc2d449e44'
-    //     );
-    //     expect(res.statusCode).toBe(200);
-    //     expect(res.body.name).toBe('Product 1');
-    //   });
-    // });
-
-    // const totalProduct = await testDB.find({}).toArray();
-
-    // expect(totalProduct).toHaveLength(2);
-    // expect(totalProduct).toEqual(
-    //   expect.arrayContaining([
-    //     {
-    //       _id: 'test-id-1',
-    //       stock_number: '1',
-    //       name: 'test-name-1',
-    //       Description: 'test-description-1',
-    //       Price: 'test-price-1',
-    //     },
-    //     {
-    //       _id: 'test-id-2',
-    //       stock_number: '2',
-    //       name: 'test-name-2',
-    //       Description: 'test-description-2',
-    //       Price: 'test-price-2',
-    //     },
-    //   ])
-    // );
+    expect(allProducts.body).toHaveLength(2);
+    expect(allProducts.body).toEqual(initialData);
   });
 
-  // it('should get a product with specific id', async () => {
-  //   console.log(await testDB.find({}).toArray());
+  it('GET /products/:id', async () => {
+    const product = await request(app).get('/products/test_id_1');
 
-  //   await request(app).get('/test-id-2').expect(200);
+    expect(product.body).toEqual({
+      _id: 'test_id_1',
+      stock_number: '1',
+      name: 'test_name_1',
+      Description: 'test_description_1',
+      Price: 'test_price_1',
+    });
+  });
 
-  //   const productWithId = await testDB.findOne({ id: 'test-id-2' });
-  //   expect(productWithId).toHaveLength(1);
-  //   expect(productWithId).toEqual({
-  //     _id: 'test-id-2',
-  //     stock_number: '2',
-  //     name: 'test-name-2',
-  //     Description: 'test-description-2',
-  //     Price: 'test-price-2',
-  //   });
-  // });
+  it('POST /products', async () => {
+    const newProduct = {
+      _id: 'test_id_3',
+      stock_number: '3',
+      name: 'test_name_3',
+      Description: 'test_description_3',
+      Price: 'test_price_3',
+    };
 
-  // it('should add a new product', async () => {
-  //   const newProduct = {
-  //     _id: 'test-id-3',
-  //     stock_number: '3',
-  //     name: 'test-name-3',
-  //     Description: 'test-description-3',
-  //     Price: 'test-price-3',
-  //   };
+    await request(app).post('/products').send(newProduct);
 
-  //   await request(app).post('/').send(newProduct).expect(200);
+    const allProducts = await productsCollection.find({}).toArray();
+    expect(allProducts).toHaveLength(3);
+    expect(allProducts).toEqual([...initialData, newProduct]);
 
-  //   const totalProduct = await testDB.find({}).toArray();
-  //   expect(totalProduct).toHaveLength(3);
+    const insertedProduct = await productsCollection.findOne({
+      _id: 'test_id_3',
+    });
+    expect(insertedProduct).toEqual(newProduct);
+  });
 
-  //   const insertedProduct = await testDB.findOne({ _id: 'test-id-3' });
-  //   expect(insertedProduct).toEqual(newProduct);
-  // });
+  it('PUT /products/:id', async () => {
+    const updateBody = {
+      _id: 'test_id_2',
+      stock_number: '2',
+      name: 'updated_test_name_2',
+      Description: 'updated_test_description_2',
+      Price: 'updated_test_price_2',
+    };
 
-  // it('should update the product with the given id', async () => {
-  //   const updateBody = {
-  //     _id: 'test-id-3',
-  //     stock_number: '3',
-  //     name: 'test-name-3',
-  //     Description: 'test-description-3',
-  //     Price: 'test-price-3',
-  //   };
+    await request(app).put('/products/test_id_2').send(updateBody);
+    const allProducts = await productsCollection.find({}).toArray();
+    expect(allProducts).toEqual([
+      {
+        _id: 'test_id_1',
+        stock_number: '1',
+        name: 'test_name_1',
+        Description: 'test_description_1',
+        Price: 'test_price_1',
+      },
+      updateBody,
+    ]);
+    expect(allProducts).toHaveLength(2);
 
-  //   await request(app).put('/test-id-3').send(updateBody).expect(200);
+    const insertedProduct = await productsCollection.findOne({
+      _id: 'test_id_2',
+    });
+    expect(insertedProduct).toEqual(updateBody);
+  });
 
-  //   const totalProduct = await testDB.find({}).toArray();
-  //   expect(totalProduct).toHaveLength(3);
+  it('DELETE /products/:id', async () => {
+    await request(app).delete('/products/test_id_2');
 
-  //   const insertedProduct = await testDB.findOne({ _id: 'test-id-3' });
-  //   expect(insertedProduct).toEqual(updateBody);
-  // });
+    const allProducts = await productsCollection.find({}).toArray();
+    expect(allProducts).toEqual([
+      {
+        _id: 'test_id_1',
+        stock_number: '1',
+        name: 'test_name_1',
+        Description: 'test_description_1',
+        Price: 'test_price_1',
+      },
+    ]);
+    expect(allProducts).toHaveLength(1);
+
+    const insertedProduct = await productsCollection.findOne({
+      _id: 'test_id_2',
+    });
+    expect(insertedProduct).toBe(null);
+  });
 });
